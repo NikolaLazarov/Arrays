ed763f75562b2e681f413452e2c42cf806df8c49
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2021-11-29 22:06:45 +0200

    Add files via upload

---
+++ Листинг 4.10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Листинг_4._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,,] symbs = new char[4, 3, 5];
+            char[] s = { 'а', 'А', 'a', 'A' };
+            for (int i = 0; i < symbs.GetLength(0); i++)
+            {
+                Console.WriteLine(" Слой №{0}:", i + 1);
+                for (int j = 0; j < symbs.GetLength(1); j++)
+                {
+                    for (int k = 0; k < symbs.GetLength(2); k++)
+                    {
+                        symbs[i, j, k] = s[i];
+                        s[i]++;
+                        Console.Write(symbs[i, j, k] + " ");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine("----------");
+            }
+            Console.ReadLine();
+        }
+    }
+}
