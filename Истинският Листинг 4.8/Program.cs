187470ccffa8d4a61e24ea5e4e78aada30bced17
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2021-11-29 21:20:58 +0200

    Add files via upload

---
+++ Истинският Листинг 4.8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Истинският_Листинг_4._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = 3, cols = 5;
+            int[,] nums = new int[rows, cols];
+            int value = 1;
+
+            for (int i = 0; i < nums.GetLength(0); i++)
+            {
+                for (int u = 0; u < nums.GetLength(1); u++)
+                {
+                    nums[i, u] = value;
+                    value++;
+
+                    Console.Write(nums[i, u] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
