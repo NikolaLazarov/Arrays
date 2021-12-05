9e06fd6ded9c845c32202cb2972fd90161d3d849
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2021-12-05 22:10:36 +0200

    Add files via upload

---
+++ Самостоятелни задачи листинг 4/Задача  1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using static System.Console;
+
+namespace Задача__1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int y = 0;
+
+            Write("How many numbers  will you enter which when  you divide by 5 you have a 2 left out: ");
+            int i = int.Parse(ReadLine());
+            int[] nums = new int[i];
+
+            Write("Enter those numbers(2,7,12,17,...): ");
+            for (int j = 0; j < i; j++)
+            {
+                nums[j] = int.Parse(ReadLine());
+                if (nums[j] % 5 != 2)
+                {
+                    WriteLine("Please enter numbers which are specified on top.");
+                    y++;
+                }
+            }
+
+            if (y > 0)
+            {
+                WriteLine();
+            }
+            else
+            {
+                WriteLine("Your array of numbers :");
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    Write(nums[j] + " ");
+                    WriteLine();
+                }
+
+            }
+            WriteLine("Press any key to close...");
+            ReadKey(true);
+        }
+    }
+}
---
+++ Самостоятелни задачи листинг 4/Задача 2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using static System.Console;
+
+namespace Задача_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[11];
+            int n = 1;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = n;
+                n = n * 2;
+            }
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Write(nums[i] + "");
+                WriteLine();
+            }
+            n=1024;
+            WriteLine("-----");
+            for (int i = 11; i < nums.Length; i--)
+            {
+                nums[i] = n;
+                n = n / 2;
+            }
+            for (int i = 10; i >= 0; i--)
+            {
+                Write(nums[i] + "");
+                WriteLine();
+            }
+
+            WriteLine("Press nay key to close...");
+            ReadKey(true);
+        }
+    }
+}
