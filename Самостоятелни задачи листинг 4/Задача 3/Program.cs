f8f31e8f4eff62d2fceb0e48c68265d8607c29d3
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2021-12-07 22:06:07 +0200

    Add files via upload

---
+++ Самостоятелни задачи листинг 4/Задача 3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using static System.Console;
+
+namespace Задача_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] symbs = new char[10];
+            char s = 'a';
+
+            for (int i = 0; i < symbs.Length; i++)
+            {
+                symbs[i] = s;
+                s++;
+                s++;
+            }
+            for (int i = 0; i < symbs.Length; i++)
+            {
+                WriteLine(symbs[i]);
+            }
+            WriteLine("-----");
+            for (int i = 9; i >= 0; i--)
+            {
+                WriteLine(symbs[i]);
+            }
+            WriteLine();
+            WriteLine("Press any key to close...");
+            ReadKey(true);
+        }
+    }
+}
