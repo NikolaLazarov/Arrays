d7868c9fcbdf07ef0f6df5166004a9232853a844
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2021-12-08 22:41:29 +0200

    Add files via upload

---
+++ Самостоятелни задачи листинг 4/Задача 4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+
+namespace Задача_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] symbs = new char[10];
+            char s = 'А';//Kirilica
+            for (int i = 0; i < symbs.Length; i++)
+            {
+                if (s == 'А' || s == 'Ъ' || s == 'О' || s == 'У' || s == 'Е' || s == 'И')
+                {
+                    s++;
+                }
+                symbs[i] = s;
+                WriteLine(symbs[i]);
+                s++;
+            }
+            WriteLine();
+
+            WriteLine("Press any key to close...");
+            ReadKey(true);
+        }
+    }
+}
