85117cb76d686b10f2fdee89acc57dbae323f9ce
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2021-12-11 00:34:32 +0200

    Add files via upload

---
+++ Самостоятелни задачи листинг 4/Задача 6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Console;
+
+namespace Задача_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[10];
+            Random rnd = new Random();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int rgn = rnd.Next(1, 20);
+                nums[i] = rgn;
+            }
+            Array.Sort(nums);
+            Array.Reverse(nums);
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Write(nums[i] + " ");
+            }
+            WriteLine();
+
+            WriteLine("Press any key to close...");
+            ReadKey(true);
+        }
+    }
+}
