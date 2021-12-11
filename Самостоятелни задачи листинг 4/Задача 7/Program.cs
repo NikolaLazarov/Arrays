07d50ace6ea48f2fe5ff47e75a42d9eec936286b
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2021-12-11 15:01:12 +0200

    Add files via upload

---
+++ Самостоятелни задачи листинг 4/Задача 7/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using static System.Console;
+
+namespace Задача_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] nums = new int[2, 3];
+            int[,] arrnums = new int[3, 2];
+            Random rnd = new Random();
+
+            WriteLine("The array :");
+            for (int i = 0; i < nums.GetLength(0); i++)
+            {
+                for (int j = 0; j < nums.GetLength(1); j++)
+                {
+                    int rgn = rnd.Next(1, 9);
+                    nums[i, j] = rgn;
+                    Write(nums[i, j] + " ");
+                }
+                WriteLine();
+            }
+
+            WriteLine("The new array :");
+            for (int i = 0; i < arrnums.GetLength(0); i++)
+            {
+                for (int j = 0; j < arrnums.GetLength(1); j++)
+                {
+                    arrnums[0, 0] = nums[1, 0];
+                    arrnums[0, 1] = nums[0, 0];
+                    arrnums[1, 0] = nums[1, 1];
+                    arrnums[1, 1] = nums[0, 1];
+                    arrnums[2, 0] = nums[1, 2];
+                    arrnums[2, 1] = nums[];
+
+                    Write(arrnums[i, j] + " ");
+                }
+                WriteLine();
+            }
+
+            WriteLine("Press any key to close...");
+            ReadKey(true);
+        }
+    }
+}
---
+++ Самостоятелни задачи листинг 4/Задача 8/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using static System.Console;
+
+namespace Задача_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arrNums = new int[3, 4];
+            Random rnd = new Random();
+
+            WriteLine("The array :");
+            for (int i = 0; i < arrNums.GetLength(0); i++)
+            {
+                for (int j = 0; j < arrNums.GetLength(1); j++)
+                {
+                    int rgn = rnd.Next(1, 9);
+                    arrNums[i, j] = rgn;
+                    Write(arrNums[i, j] + " ");
+                }
+                WriteLine();
+            }
+
+            WriteLine("The new array :");
+            int[,] newnArr = new int[arrNums.GetLength(0) - 1, arrNums.GetLength(1) - 1];
+            for (int i = 0; i < newnArr.GetLength(0); i++)
+            {
+                for (int j = 0; j < newnArr.GetLength(1); j++)
+                {
+                    newnArr[i, j] = arrNums[i, j];
+                    Write(newnArr[i, j] + " ");
+                }
+                WriteLine();
+            }
+
+            WriteLine("Press any key to exit...");
+            ReadKey(true);
+        }
+    }
+}
